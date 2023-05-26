d48c953173d77ed1e72525ce9f8d889318f02345
Marat <almabekov.marat@gmail.com>
2023-05-27 01:12:55 +0600

    Minor updates.

--- app/src/main/java/com/demoapp/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/demoapp/photogallery/PhotoGalleryFragment.kt
@@ -1,13 +1,22 @@
 package com.demoapp.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import com.demoapp.photogallery.api.FlickrApi
 import com.demoapp.photogallery.databinding.FragmentPhotoGalleryBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
 
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
@@ -15,6 +24,25 @@ class PhotoGalleryFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        val flickrHomePageRequest = flickrApi.fetchContents()
+
+        flickrHomePageRequest.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+        })
     }
 
     override fun onCreateView(
