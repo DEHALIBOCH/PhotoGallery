0f297f39fce601646e883b14765602e3c2359de8
Marat <almabekov.marat@gmail.com>
2023-05-27 23:18:18 +0600

    Adding FlickrFetcher - Repository template for connecting to the flickr api using retrofit.

--- app/src/main/java/com/demoapp/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/demoapp/photogallery/PhotoGalleryFragment.kt
@@ -2,19 +2,15 @@ package com.demoapp.photogallery
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
-import com.demoapp.photogallery.api.FlickrApi
+import com.demoapp.photogallery.api.FlickrFetcher
 import com.demoapp.photogallery.databinding.FragmentPhotoGalleryBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.create
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -25,23 +21,9 @@ class PhotoGalleryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-
-        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
-
-        val flickrHomePageRequest = flickrApi.fetchContents()
-
-        flickrHomePageRequest.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                Log.d(TAG, "Response received: ${response.body()}")
-            }
-
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.e(TAG, "Failed to fetch photos", t)
-            }
+        val flickrLiveData: LiveData<String> = FlickrFetcher().fetchContents()
+        flickrLiveData.observe(this, Observer { responseString ->
+            Log.d(TAG, "Response received: $responseString")
         })
     }
 
