ad5b65e9170b501bc11d8a3326997681107a6d35
Marat <almabekov.marat@gmail.com>
2023-05-29 00:37:06 +0600

    Interceptor for Retrofit and function to search photos

--- app/src/main/java/com/demoapp/photogallery/api/FlickrFetcher.kt
+++ app/src/main/java/com/demoapp/photogallery/api/FlickrFetcher.kt
@@ -7,6 +7,7 @@ import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.demoapp.photogallery.GalleryItem
+import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -22,17 +23,30 @@ class FlickrFetcher {
     private val baseApiUrl = "https://api.flickr.com/"
 
     init {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(baseApiUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
 
     fun fetchPhotos(): LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.fetchPhotos())
+    }
+
+    fun searchPhotos(query: String): LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.searchPhotos(query))
+    }
+
+    private fun fetchPhotoMetadata(flickrRequest: Call<FlickrResponse>)
+            : LiveData<List<GalleryItem>> {
         val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
-        val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos()
 
         flickrRequest.enqueue(object : Callback<FlickrResponse> {
             override fun onResponse(
@@ -58,7 +72,7 @@ class FlickrFetcher {
     }
 
     @WorkerThread
-    fun fetchPhoto(url: String) : Bitmap? {
+    fun fetchPhoto(url: String): Bitmap? {
         val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
         val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
         Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
